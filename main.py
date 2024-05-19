398d3dd15ee10fba9699ce7c557413398a7c331b
Diedrik <diedrik@hotmail.com>
2024-05-19 12:11:11 +0200

    Edit to use FFT with threshold to clean noisy signal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import ruamel.yaml
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq
+from scipy.fft import fft, fftfreq, ifft
 
 from signal import smooth_signal, random_noise
 
@@ -10,7 +10,7 @@ with open("config.yaml", "r") as stream:
         yaml = ruamel.yaml.YAML(typ='rt')
         settings = yaml.load(stream)
 
-    except yaml.YAMLError as exc:
+    except ruamel.yaml.YAMLError as exc:
         print(exc)
         exit(1)
 
@@ -24,15 +24,44 @@ series = settings["timeseries"]
 t = np.arange(series["start"], series["end"], series["resolution"])
 size = t.size
 
+fft_threshold = settings["FFT"]["threshold"]
+
+# Initializing signal and noise
 signal = smooth_signal(t)
 noise = random_noise(size, seed, mu, sigma)
 noisy_signal = signal + noise
 
-plt.plot(t, noisy_signal)
-
+fft_pure = fft(signal)
 fft_signal = fft(noisy_signal)
 fft_x = fftfreq(size, series["resolution"])
 
+fft_cleaned = np.where(abs(fft_signal) < fft_threshold, 0, fft_signal)
+cleaned_signal = ifft(fft_cleaned)
+
+
+# Plotting signal and noise
+plt.plot(t, noisy_signal, label='Noisy')
+plt.plot(t, cleaned_signal, label='Cleaned')
+plt.plot(t, signal, '--', label='Original')
+plt.legend()
+plt.title("Signal with noise")
+plt.xlabel("Time")
+plt.ylabel("Signal")
+
 plt.figure()
-plt.semilogy(fft_x, abs(fft_signal))
+plt.semilogy(fft_x, abs(fft_signal), label="Noisy")
+plt.semilogy(fft_x, abs(fft_cleaned), label='Cleaned')
+plt.semilogy(fft_x, abs(fft_pure), '--', label='Pure')
+plt.legend()
+plt.title("FFT of signal with and without noise")
+plt.xlabel("Frequency")
+plt.ylabel("Occurence")
+
+
+plt.figure()
+plt.hist(noise)
+plt.title("Distribution of noise")
+plt.xlabel("Noise strength")
+plt.ylabel("Occurence")
+
 plt.show(block=True)
